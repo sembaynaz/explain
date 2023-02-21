48116b73df6a4a0cef51b53211344b85b65185e4
Naz <nazerke.sembay11@mail.ru>
2023-02-21 18:13:14 +0600

    Initial Commit

---
+++ 14Topic MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  14Topic MVVM
+//
+//  Created by Nazerke Sembay on 21.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
